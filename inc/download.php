3ca7e35ce76b249c1b2aab28f7f309832adf47fe
Niko Sams <niko.sams@gmail.com>
2010-05-02 11:38:49 +0200

    Add download quickbox

---
+++ inc/download.php
@@ -0,0 +1,12 @@
+<?php
+module_head('Download', 'cat-download');
+echo "<p>Latest release:</p>\n";
+echo "<a href=\"#\" class=\"download\">KDevelop 4.0.0<br /><span class=\"date\">May 1, 2010</span></a>\n";
+echo "<a href=\"#\" class=\"download download-legacy\">KDevelop 3.5.0<br /><span class=\"date\">Aug 11, 2009 (legacy release)</span></a>\n";
+echo "<p>More:</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">Other releases, Plugins</a></li>\n";
+echo "<li><a href=\"#\">Latest sources from SVN</a></li>\n";
+echo "</ul>\n";
+module_tail();
+?>
