e1f55e4d2a3a89aabc1079ff5d81f3c105f62e7a
Niko Sams <niko.sams@gmail.com>
2010-05-02 13:52:17 +0200

    Rounded borders for content boxes.
    Implemented with lot's of empty divs.

--- inc/theme4.php
+++ inc/theme4.php
@@ -1,11 +1,17 @@
 <?php
 
 function module_head($subject, $class=''){
-
-echo '<div class="moduleentry '.$class.'">
-  <div class="header"><img class="category-icon"/><h3>'.$subject.'</h3></div>
-  <div class="content">
-';
+    echo "<div class=\"moduleentry $class\">\n";
+    if ($class == 'border') {
+        echo "<div class=\"border-tl\"></div>\n";
+        echo "<div class=\"border-tr\"></div>\n";
+        echo "<div class=\"border-bl\"></div>\n";
+        echo "<div class=\"border-br\"></div>\n";
+        echo "<div class=\"border-l\"></div>\n";
+        echo "<div class=\"border-r\"></div>\n";
+    }
+    echo "<div class=\"header\"><img class=\"category-icon\"/><h3>$subject</h3></div>\n";
+    echo "<div class=\"content\">\n";
 }
 
 function module_tail(){
