0352a1f19c9b0e74e3ea8dbd6cc96d06b1c24904
Niko Sams <niko.sams@gmail.com>
2010-05-02 17:14:01 +0200

    Add Support and Participate boxes

---
+++ inc/participate.php
@@ -0,0 +1,18 @@
+<?php
+module_head('How to Participate', 'cat-participate');
+echo "<p>Programming</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">join the development team</a></li>\n";
+echo "</ul>\n";
+echo "<p>Translating</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">translate KDevelop &amp; plugins</a></li>\n";
+echo "<li><a href=\"#\">translate this website</a></li>\n";
+echo "</ul>\n";
+echo "<p>More</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">improve the documentation</a></li>\n";
+echo "<li><a href=\"#\"><em>other tasks / ideas</em></a></li>\n";
+echo "</ul>\n";
+module_tail();
+?>
---
+++ inc/support.php
@@ -0,0 +1,17 @@
+<?php
+module_head('Support', 'cat-support');
+echo "<p>Learn to user KDevelop:</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">User Manual</a></li>\n";
+echo "<li><a href=\"#\">FAQ</a>, <a href=\"#\">Tutorials</a>, <a href=\"#\">Tips &amp; Tricks</a></li>\n";
+echo "</ul>\n";
+echo "<p>Ask Questions:</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">Forum</a>, <a href=\"#\">Chat</a>, <a href=\"#\">Mailinglist</a></li>\n";
+echo "</ul>\n";
+echo "<p>Report Bugs:</p>\n";
+echo "<ul>\n";
+echo "<li><a href=\"#\">Bug Tracker</a></li>\n";
+echo "</ul>\n";
+module_tail();
+?>
